80a7698d465edd1cfd6436f232e333eafbd4f0e1
IvanYakush <ivan.yakushevsky@gmail.com>
2019-03-12 19:49:25 +0200

    Add sanitize.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './App.css';
 
 
 class App extends Component {
   render() {
     return (
-      <p>Hello World</p>
+      <p>Hello World!</p>
     );
   }
 }
