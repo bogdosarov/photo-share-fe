51af03b1944ffa6b3ee9e630e9cd67ecab63e797
Ihor Matsiuk <imatsiuk@cogniance.com>
2019-03-16 13:45:32 +0200

    Setup prettier, eslint, precommit.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
-    return (
-      <p>Hello World!</p>
-    );
+    return <p>Hello World!</p>
   }
 }
 
-export default App;
+export default App
