22077d6d92048204d95d1bb80bfe65eedb185019
anton <anton.smolin@lindenvalley.de>
2019-03-17 22:48:45 +0200

    Add component Sidebar

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import { PostList } from 'components/PostList/PostList'
+import Sidebar from '../../components/Sidebar/Sidebar'
 
 const Home = () => (
   <div className="conteiner">
     <h1>Home</h1>
-    <PostList />
+    <main className="main-content">
+      <PostList />
+      <Sidebar />
+    </main>
   </div>
 )
 
