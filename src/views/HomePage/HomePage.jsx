4cfc9fd1e8f2491f010bbc09c34aad96299aad6c
anton <anton.smolin@lindenvalley.de>
2019-03-20 21:55:04 +0200

    Update templates after first review. path 3

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -1,10 +1,10 @@
 import React, { Fragment } from 'react'
 
-import styles from './HomePage.module.scss'
-
 import { PostList } from 'components/PostList/PostList'
 import { PageSidebar } from 'components/PageSidebar/PageSidebar'
 import { PageHeader } from 'components/PageHeader/PageHeader'
+
+import styles from './HomePage.module.scss'
 
 export const HomePage = () => (
   <Fragment>
